b5c3abc0ad10a3df4f3bcdc498165afd0d6b0b52
malakelsayyad <malakelsayyad@yahoo.com>
2024-12-12 17:22:19 +0200

    Assignment solution

---
+++ Assignment01/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace Assignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //Console.WriteLine("Enter a Number");
+            //int Number = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Your Number is : " + Number);
+            #endregion
+
+            #region Q2
+            //string Alphabets = "abc";
+            //int Result;
+
+            //Console.WriteLine(int.TryParse(Alphabets, out Result));
+            //Console.WriteLine(Result); // false 0
+            #endregion
+
+            #region Q3
+            //float Number1 = 1.2f, Number2 = 2.5f;
+            //Console.WriteLine("Sum is : " + (Number1+Number2)); // Sum is : 3.7
+            #endregion
+
+            #region Q4
+            //string Input, NewSub;
+            //Input = "Hello world";
+
+
+            //NewSub = Input.Substring(0,5);
+
+            //Console.WriteLine("The substring is : " + NewSub);
+            #endregion
+
+            #region Q5
+            //int X = 5;
+            //int Y = X;
+
+            //Y = 6 ;
+            //Console.WriteLine(" X : " + X); // 5
+            //Console.WriteLine(" Y : " + Y); // 6
+            #endregion
+
+            #region Q6
+            //Point P01;
+
+            //P01 = new Point();
+
+            //Point P02 = new Point();
+            //P02.X = 12;
+
+            //Console.WriteLine("P01.X before modifying P02.X is : " + P01.X);
+
+            //P01 = P02;
+            //Console.WriteLine("P01.X after modifying P02.X is : " + P01.X);
+            #endregion
+
+            #region Q7
+            //string FirstString, SecondString;
+            //Console.WriteLine("Enter the first string");
+            // FirstString = Console.ReadLine();
+            //Console.WriteLine("Enter the Second string");
+            // SecondString = Console.ReadLine();
+
+            //Console.WriteLine("Concatenated String is : " + FirstString + " " + SecondString );
+            #endregion
+
+            #region Q8
+            //float Princpal, Rate, Time ;
+
+            //Console.WriteLine("Enter Principal amount");
+            //Princpal = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter Rate");
+            //Rate = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter Time");
+            //Time = float.Parse(Console.ReadLine());
+
+            //float Interest = (Princpal * Rate * Time) / 100;
+
+            //Console.WriteLine("Interest : " + Interest);
+            #endregion
+
+            #region Q9
+            //float Weight, Height;
+
+            //Console.WriteLine("Enter your weight in kilograms");
+            //Weight = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter your height in meters");
+            //Height = float.Parse(Console.ReadLine());
+
+            //float BodyMass = (Weight) / (Height * Height);
+            //Console.WriteLine("Body mass : " + BodyMass);
+            #endregion
+
+            #region Q10
+            //Console.WriteLine("Enter the temperature:");
+            //int temperature = Convert.ToInt32(Console.ReadLine());
+
+            //string Result = (temperature < 10)? "Just Cold" :(temperature>30)?"Just Hot":"Just Good";
+
+            //Console.WriteLine(" The temperature is : " +Result);
+            #endregion
+
+            #region Q12
+            // output is The event is on c) 06/14/2024
+            #endregion
+
+            #region Q13
+            // A value 1 will be assigned to d.
+            #endregion
+
+            #region Q14
+            // 6 1
+            #endregion
+
+            #region Q15
+            // 7 7
+            #endregion
+        }
+    }
+}
